7136c76146441c202b063e25a14f9c1335700fc9
Uian Sol <sol.uian@gmail.com>
2016-05-18 00:48:26 -0300

    Update consultas_grupo05.sql

--- consultas_grupo05.sql
+++ consultas_grupo05.sql
@@ -1,49 +1,54 @@
-/*1 - Liste a quantidade de alunos por linha de pesquisa.*/
+/*Consulta 5: Fa√ßa um trigger que permita ao remover um aluno de p√≥s sejam removidas suas supervis√µes*/
+CREATE or REPLACE TRIGGER remove_supervisor 
+  AFTER DELETE ON aluno
+  FOR EACH ROW 
+BEGIN 
+  DELETE FROM graduacao g 
+  WHERE g.supervisor = :OLD.matr;
+END;
 
+/*1 - Liste a quantidade de alunos por linha de pesquisa.*/
 SELECT lp.nomearea, lp.nomesubarea, COUNT(*)
 FROM linhaPesquisa lp, aluno a
 WHERE lp.codarea = a.codarea and lp.codsubarea = a.codsubarea
 GROUP BY lp.nomearea, lp.nomesubarea
 
 /*2 - Liste os professores com menos de 30 anos que coordenam projetos.*/
-
 select p.nome
 from pessoa p, professor pr, projeto proj
 where p.matr = pr.matr and pr.matr = proj.coordenador and months_between( CURRENT_DATE, p.datanasc)/12 < 30
 
-/*Consulta 3: Crie uma view que lista os projetos que n„o geraram patentes ou publicaÁıes.*/
-
+/*Consulta 3: Crie uma view que lista os projetos que n√£o geraram patentes ou publica√ß√µes.*/
 CREATE OR REPLACE VIEW [Projetos sem publicacoes ou patentes] AS
 SELECT *
 FROM projeto p
 WHERE p.cod NOT IN (SELECT pub.projeto FROM publicacao pub) 
 OR p.cod NOT IN (SELECT pat.projeto FROM patente pat)
 
-/*Consulta 5: FaÁa um trigger que permita ao remover um aluno de pÛs sejam removidas suas supervisıes*/
 
 
-/*Consulta 7: Crie uma view que liste os laboratÛrios que possuem mais recursos que a mÈdia dos outros laboratÛrios.*/
+/*Consulta 7: Crie uma view que liste os laborat√≥rios que possuem mais recursos que a m√©dia dos outros laborat√≥rios.*/
 
-/*8 - Quais laboratÛrios n„o possuem alunos de doutorado?*/
+/*8 - Quais laborat√≥rios n√£o possuem alunos de doutorado?*/
 
 SELECT lab.nome
 FROM laboratorio lab, projeto_laboratorio pl, pos p, aluno a
 WHERE lab.cod = pl.laboratorio and pl.projeto = a.projeto and a.matr = p.aluno and p.titulo != 'doutorando'
 
-/*Consulta 9: Quais laboratÛrios apresentam menos recursos?*/
+/*Consulta 9: Quais laborat√≥rios apresentam menos recursos?*/
 SELECT *
 FROM laboratorio l
 
-/*10 - Quantos alunos com "Jo„o" no nome nasceram em cada ano a partir de 1990?*/
+/*10 - Quantos alunos com "Jo√£o" no nome nasceram em cada ano a partir de 1990?*/
 
 SELECT EXTRACT(YEAR FROM p.datanasc) as ANO , COUNT(*) as QUANTIDADE_ALUNOS
 FROM aluno a, pessoa p
-WHERE p.nome LIKE '%Jo„o%' and a.matr = p.matr and EXTRACT(YEAR FROM p.datanasc) > 1989
+WHERE p.nome LIKE '%Jo√£o%' and a.matr = p.matr and EXTRACT(YEAR FROM p.datanasc) > 1989
 GROUP BY EXTRACT(YEAR FROM p.datanasc)
 
-/*Consulta 11: Quais alunos tiveram mais publicaÁıes nos eventos da IEEE?*/
+/*Consulta 11: Quais alunos tiveram mais publica√ß√µes nos eventos da IEEE?*/
 
-/*12. Qual o departamento com mais professores e a mÈdia de idade destes professores? a media ta com problema ainda nao arrumei*/
+/*12. Qual o departamento com mais professores e a m√©dia de idade destes professores? a media ta com problema ainda nao arrumei*/
 
 SELECT pr.departamento, AVG(months_between( CURRENT_DATE, p.datanasc)/12) as IDADE_MEDIA
 FROM professor pr, pessoa p
@@ -53,7 +58,7 @@ HAVING COUNT(*) >= ALL
 (SELECT COUNT(*) FROM professor GROUP BY departamento)
 
 
-/*Consulta 13: Liste o n˙mero de professores que orientam algum aluno de mestrado por departamento.*/
+/*Consulta 13: Liste o n√∫mero de professores que orientam algum aluno de mestrado por departamento.*/
 
 /*14. Liste os 2 professores que mais coordenam projetos.*/
 
@@ -66,7 +71,7 @@ ORDER BY COUNT(*) DESC )
 WHERE ROWNUM <= 2;
 
 
-/*Consulta 15: Liste os alunos que est„o envolvidos em projetos n„o associados a sua linha de pesquisa.*/
+/*Consulta 15: Liste os alunos que est√£o envolvidos em projetos n√£o associados a sua linha de pesquisa.*/
 
 
 /**/
